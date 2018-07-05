add45665cfb7cccdd31dab6ed75354776549b526
SubMishMar <subodheenitr@gmail.com>
2018-07-05 00:28:23 -0700

    solved Q1

--- chapter3/question1/main_file.m
+++ chapter3/question1/main_file.m
@@ -1,17 +1,47 @@
-%% Clear Stuff
-clear;
-clc;
-
-%% Initialize
-
+%%
+clear all
+clc
+%%
 X = zeros(2, 1);
-Xtrue =  X;
-u = zeros(1, 1);
 P = zeros(2, 2);
-std = 1; % standard deviation of ip
-mu = 0; % mean of ip
-for t = 1:5
-    [X, P] = motion_model(X, mu, P);
-    [Xtrue, ~] = motion_model(Xtrue,... 
-                              std*randn(1,1)+ mu, P);
+X_true = zeros(2, 1);
+
+
+tend = 5;
+std = 1;
+
+r = [0.4; 0.2];
+R = diag(r.^2);
+
+q = sqrt(10);
+Q = diag(q.^2);
+
+dt = 1;
+At = [1 dt;
+      0 1];
+Bt = [dt^2/2; dt];
+Ct = [1 0];
+
+
+for t = 1:tend
+    u = std*randn(1,1); % acc ip with zero mean
+                        % and std = 1
+    X_true = At*X_true + Bt*u + r.*randn(2, 1);
+    
+    X = At*X + Bt*u;
+    P = At*P*At' + R;
+    [Xdraw, Ydraw] = covarianceEllipse(X, P, 1, 50);
+    figure(1)
+    plot(Xdraw, Ydraw);
+    hold on;
+    plot(X(1), X(2), '+');
+    hold on;
+    plot(X_true(1), X_true(2), '*');
+    hold off;
+    grid;
+    axis square;
+    axis([-10 10 -10 10]);
+    pause(0.1);
+    drawnow;
 end
+
