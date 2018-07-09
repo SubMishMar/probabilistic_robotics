47c1bc719f6f695543418dfefcc73d12e8b14000
SubMishMar <subodheenitr@gmail.com>
2018-07-08 23:04:04 -0700

    added bar plot

--- chapter4/discrete_bayes_filter/OneDWorld_Localization/main_file.m
+++ chapter4/discrete_bayes_filter/OneDWorld_Localization/main_file.m
@@ -6,7 +6,14 @@ p = [0.2, 0.2, 0.2, 0.2, 0.2]; % initial belief
 measurements = ["red", "red"];
 motions = [1, 1];
 
+figure(1);
+bar(p);
+pause(1)
 for i = 1:length(measurements)
     p = sense(p, measurements(i), world);
     p = move_inexact(p, motions(i));
+    bar(p)
+    pause(1);
 end
+
+
