1b0dfbadb7d431e0a2d7a8bba2be633d9e989993
SubMishMar <subodheenitr@gmail.com>
2018-07-15 17:52:03 -0700

    PF implementation

--- chapter4/particle_filters/main_file.m
+++ chapter4/particle_filters/main_file.m
@@ -2,6 +2,9 @@ clear all;
 close all;
 clc;
 
+disp('Basic Implementation of Particle Filter for Robot Localization');
+disp('The filter does not always converge to the correct solution')
+disp('So re run the code if it doesnt converge to the truth');
 %% world parameters 
 
 world_size = 100;
@@ -17,67 +20,107 @@ turn_motion_noise = 0.05; % std/not variance!
 
 control_noise = [forward_motion_noise;
                   turn_motion_noise];
-sensor_noise = 5.0; % std/not variance!
+sensor_noise = 10.0; % std/not variance!
 
 %% robot initialization
 
-no_of_iterations = 50;
+no_of_iterations = 1000;
+delP = 1;
+delOmega = 0*pi/180;
+control_ip = [delP; delOmega];
 
-X_true = zeros(3, no_of_iterations);
-x_true = X_true(:,1);
-z_true = zeros(size(landmark_locations, 1), no_of_iterations);
-control_ip = [1; 0];
-
-%% Generate Truth
-
-for i = 1:no_of_iterations
-    z_true(:,i) = sense(x_true, landmark_locations, sensor_noise);
-    x_true = move(x_true, control_ip, control_noise, world_size);
-    X_true(:,i) = x_true; 
-end
 
 %% Particle Filter
 % Note that the only thing accecible to the particle filter is
 % the measurement z_true and control_ip
-% N = 1000; % No of particles
-% 
-% population = 0:0.001:world_size;
-% population_theta = -180:0.001:180;
-% 
-% X_samples = [randsample(population, N, true); 
-%              randsample(population, N, true); 
-%              randsample(population_theta, N, true)];
-%      
-% figure(1);
-% for i = 1:no_of_iterations
-%     plot(X_samples(1,:), X_samples(2,:),'o',...
-%                                 'MarkerSize',5,...
-%                                 'MarkerEdgeColor','r',...
-%                                 'MarkerFaceColor',[1,0,0]);
-%     hold on;
-% 
-%     plot(landmark_locations(:,1), landmark_locations(:,2),'s',...
-%                                 'MarkerSize',25,...
-%                                 'MarkerEdgeColor','r',...
-%                                 'MarkerFaceColor',[0,1,0])
-%     hold off;
-%     axis square;
-%     xlim([0 100]);
-%     ylim([0 100]);
-%     grid;
-%     prob = zeros(length(X_samples), 1);
-%     for j = 1:N
-%         predicted_measurement = sense(X_samples(:,j), landmark_locations, 0);
-%         
-%         prob(j) = measurement_probability(predicted_measurement, ... 
-%                                         z_true, sensor_noise);
-%         X_samples(:,j) = move(X_samples(:,j), control_ip, zeros(2, 1), world_size);
-%     end
-%     prob = prob/sum(prob);
-%     indexes = randsample(1:N, N,true,prob);
-%     X_samples = X_samples(:,indexes);
-%     pause(0.1);
-% end
+N = 1000; % No of particles
+
+population = 0:0.001:world_size;
+population_theta = 0:0.001:360;
+
+X_samples = [randsample(population, N, true); 
+             randsample(population, N, true); 
+             randsample(population_theta, N, true)];
+
+x_true = [randsample(population, 1, true);
+          randsample(population, 1, true); 
+          randsample(population_theta, 1, true)];
+
+figure(1);
+for i = 1:no_of_iterations
+    x_true = move(x_true, control_ip, control_noise, world_size);
+    z_true = sense(x_true, landmark_locations, sensor_noise);
+    for j = 1:N
+        
+        X_samples(:,j) = move(X_samples(:,j), control_ip, control_noise, world_size);
+        predicted_measurement = sense(X_samples(:,j), landmark_locations, 0); 
+        prob(j) = measurement_probability(predicted_measurement, z_true, sensor_noise);
+    
+    end
+    
+    prob = prob/sum(prob);
+    index = randsample(1:N, 1);
+    
+    % Resampling procedure
+    beta = 0.0;
+    X_samples_temp = zeros(size(X_samples));
+    probMax = max(prob);
+    
+    for j = 1:N
+        
+        beta = beta + 2*probMax*rand(1, 1);
+
+        while(beta > prob(index))
+            beta = beta - prob(index);
+            index = index+1;
+            if (index > N)
+                index = index - N;
+            end
+        end
+        X_samples_temp(:,j) = X_samples(:, index);
+        
+    end
+    
+    X_samples = X_samples_temp;
+    
+    measurement_lineAx = linspace(landmark_locations(1,1), x_true(1), 100);
+    measurement_lineAy = linspace(landmark_locations(1,2), x_true(2), 100);
+    measurement_lineBx = linspace(landmark_locations(2,1), x_true(1), 100);
+    measurement_lineBy = linspace(landmark_locations(2,2), x_true(2), 100);
+    measurement_lineCx = linspace(landmark_locations(3,1), x_true(1), 100);
+    measurement_lineCy = linspace(landmark_locations(3,2), x_true(2), 100);
+    measurement_lineDx = linspace(landmark_locations(4,1), x_true(1), 100);
+    measurement_lineDy = linspace(landmark_locations(4,2), x_true(2), 100);
+    plot(measurement_lineAx, measurement_lineAy, '--r');
+    hold on;
+    plot(measurement_lineBx, measurement_lineBy, '--r');
+    hold on;
+    plot(measurement_lineCx, measurement_lineCy, '--r');
+    hold on;
+    plot(measurement_lineDx, measurement_lineDy, '--r');
+    hold on;
+    plot(landmark_locations(:,1), landmark_locations(:,2),'s',...
+                                'MarkerSize',25,...
+                                'MarkerEdgeColor','r',...
+                                'MarkerFaceColor',[0,1,0])
+    hold on;
+    plot(X_samples(1,:), X_samples(2,:),'o',...
+                                'MarkerSize',1,...
+                                'MarkerEdgeColor','b',...
+                                'MarkerFaceColor',[0,0,1])
+    hold on;
+    plot(x_true(1), x_true(2),'o',...
+                                'MarkerSize',5,...
+                                'MarkerEdgeColor','r',...
+                                'MarkerFaceColor',[1,0,0]);
+    hold off;
+    prob = zeros(1, N);
+    axis square;
+    xlim([0 100]);
+    ylim([0 100]);
+    grid;
+    pause(0.1);   
+end
 
 
 
--- chapter4/particle_filters/move.m
+++ chapter4/particle_filters/move.m
@@ -7,19 +7,19 @@ function X = move(X, control_ip, control_noise, world_size)
        return;
    end
    
-   angular_motion = control_ip(2)*pi/180; % input is in degrees
+   angular_motion = control_ip(2); % input is in degrees
    
    forward_motion_noise = control_noise(1);
-   angular_motion_noise = control_noise(2)*pi/180;
+   angular_motion_noise = control_noise(2);
    
    theta = X(3);
    theta = theta + angular_motion + angular_motion_noise*randn(1,1);
-   theta = atan2(sin(theta), cos(theta));
+   theta = mod(theta, 2*pi);
    
    forward_motion = forward_motion + forward_motion_noise*randn(1,1);
    
    X(1:2,:) = X(1:2,:) + [cos(theta); sin(theta)]*forward_motion;
    X(1) = mod(X(1), world_size);
    X(2) = mod(X(2), world_size);
-   X(3) = theta*180/pi;
+   X(3) = theta;
 end
